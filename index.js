564958af816ea37c3627f4dbaa04099993c9ca7d
brighthas <brighthas>
2013-06-29 16:43:04 +0800

    fixed scale

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 module.exports = function(canvas,windowX,windowY){
-    var bbox = canvas.getBoundingClientRect();
+    var bbox = canvas.getBoundingClientRect(),
+        w_scale = canvas.width/bbox.width,
+        h_scale = canvas.height/bbox.height;
+        
     return {
-        x:windowX-bbox.left * (canvas.width/bbox.width),
-        y:windowY-bbox.top * (canvas.height/bbox.height)
+        x:windowX * w_scale - bbox.left * w_scale,
+        y:windowY * h_scale -bbox.top * h_scale
     }
 }
